348e27d8ab6aad28ada8356d67b05e9cdcd29a66
suguMaranram <sugumaran.ram@aspiresys.com>
2024-05-28 11:00:53 +0530

    feat: Implement user login functionality
    This commit introduces a new login component to the application, enabling users to authenticate with their credentials. The component features a reactive form with validation for username (email) and password. Upon submission, it sends a POST request to the specified endpoint for server-side authentication.

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm!:FormGroup;
+  public showPassword:boolean=false;
+  constructor(private fb:FormBuilder,private http:HttpClient)
+  {
+    
+  }
+  ngOnInit():void{
+    this.loginForm=this.fb.group({
+      username:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(8)]]
+
+    })
+
+  }
+
+  public togglePasswordVisibility():void
+  {
+    this.showPassword=!this.showPassword
+  }
+  public login():void
+  {
+    console.log(this.loginForm.value)
+    this.http.post('http://192.168.43.173',this.loginForm.value).subscribe((res)=>
+    {
+    },(err)=>{
+    alert(`Error ${err}`)
+    });
+  }
+
+}
