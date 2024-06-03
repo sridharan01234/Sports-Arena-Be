2eddb3183cf33b33263e909cb07cf8468fd6e7fd
suguMaranram <sugumaran.ram@aspiresys.com>
2024-06-03 11:58:37 +0530

    header spec file

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,21 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [HeaderComponent]
-    });
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  
 });
