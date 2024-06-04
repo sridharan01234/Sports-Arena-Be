5fe77e88a209c14d38fee5e648b38a9f01a4d625
vinushanmugam16 <vinu.shanmugam@aspiresys.com>
2024-06-04 12:25:24 +0530

    products-view

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductViewComponent } from './product-view/product-view.component';
+import { ProductComponent } from './product.component';
 
-const routes: Routes = [{ path: 'product/view', title:'Product-View' ,component: ProductViewComponent }];
+const routes: Routes = [
+  { path: 'product/view/:id', title: 'Product-View', component: ProductViewComponent },
+  { path: 'product', title: "Product", component: ProductComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
