f72223b2814838b6fd971efe4d5851fad3014e74
suguMaranram <sugumaran.ram@aspiresys.com>
2024-06-04 12:34:29 +0530

    products

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductViewComponent } from './product-view/product-view.component';
+import { ProductComponent } from './product.component';
 
-const routes: Routes = [{ path: 'product/view', title:'Product-View' ,component: ProductViewComponent }];
+const routes: Routes = [
+  {path:'product',title:'Products',component:ProductComponent},
+  { path: 'product/view', title:'Product-View' ,component: ProductViewComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
